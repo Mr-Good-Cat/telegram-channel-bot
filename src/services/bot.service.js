80486d071a931a1317a7cba67114e748f3f69098
ivan.sankov <sii200192@gmail.com>
2023-08-29 16:46:14 +0700

    create bot service to send file into telegram

---
+++ src/services/bot.service.js
@@ -0,0 +1,27 @@
+const FormData = require("form-data");
+const BotTelegram = require("../libs/telegram/bot.telegram");
+
+class BotService {
+  #telegramClient = new BotTelegram();
+
+  sendPhoto({ fileName, stream }) {
+    const formData = this.#prepareInputFile({ fileName, stream });
+
+    return this.#telegramClient.sendPhoto(formData);
+  }
+
+  #prepareInputFile({ fileName, stream }) {
+    const formData = new FormData();
+
+    formData.append("chat_id", process.env.TELEGRAM_CHANNEL_CHAT_ID);
+    formData.append("photo", stream);
+    formData.append(
+      "caption",
+      "#" + fileName.split(".")[0].replace(/\s+/, "_"),
+    );
+
+    return formData;
+  }
+}
+
+module.exports = BotService;
